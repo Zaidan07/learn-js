393a610d2c1739a5761600fb4a55d16cdd492981
Zaidan07 <zaidanxgaming@gmail.com>
2024-06-18 16:07:42 +0700

    feat: first

---
+++ index.js
@@ -0,0 +1,97 @@
+// menggunakan variable const data didalamnya tidak bisa diubah
+// menggunakan variabke let data didalamnya dapat diubah
+// menggunakan variable var data di dalamnya dapat diubah dan bersifat  
+
+
+
+// const nama = "Joko";
+// const umur = 15;
+// const menikah = true;
+// const berat_badan = 60.8
+// const anak = ["tono", "wawan", "udin","hanif"];
+// const all = {
+//     nama: nama,
+//     umur: umur,
+//     berat_badan: berat_badan,
+
+
+// }
+
+// console.log(nama);
+
+// const negaraFav = ["Indonesia", "Jepang", "Jerman"]
+
+// console.log(negaraFav[0]);
+
+// const biodata = {
+//     name: "Muhammad Zaidan Nabih",
+//     age: 18,
+//     hobby: ["Membaca Novel", "Menulis Novel", "Badminton"],
+//     isMarried: false,
+    
+// }
+
+// console.log(biodata.name);
+// console.log(biodata.age);
+// console.log(biodata.hobby[0]);
+// console.log(biodata.isMarried);
+
+// const students = [
+//     {
+//         name: "Habib",
+//         age: 17,
+//         isMarried: false,
+//         hobby: ["badminton", "bermain bola"]
+//     },
+//     {
+//         name: "Shabri",
+//         age: 18,
+//         isMarried: true,
+//         hobby: ["nonton film","main game"]
+//     },
+//     {
+//         name: 'Abby',
+//         age: 18,
+//         isMarried: true,
+//         hobby: ["main game", "nonton anime"]
+//     },
+//     {
+//         name: 'Rafi',
+//         age: 17,
+//         isMarried: false,
+//         hobby: ["main game", "3d"]
+//     },
+//     {
+//         name: 'Yudha',
+//         age: 17,
+//         isMarried: false,
+//         hobby: ["bermain game"]
+//     },
+// ]
+
+// for(let i = 0; i < students.length; i++){    for  <--- sering digunakan pada backend
+//     console.log(students[i]);
+// }
+
+// function Tambah(a,b) {                      function <--- sering digunakan pada frontend   
+//     console.log(a + b);
+// }
+//  function Kurang(a, b){
+//     console.log(a - b);
+//  }
+
+// function Bagi(a, b){
+//     console.log(a / b );
+// }
+// function Kali(a, b){
+//     console.log(a * b);
+// }
+// function RataRata(a, b, c){
+//     console.log((a + b + c) / 3)
+// }
+
+// Tambah(2, 3)
+// Kurang(3, 4)
+// Bagi(4, 5)
+// Kali(5, 6)
+// RataRata(6, 7, 8)
